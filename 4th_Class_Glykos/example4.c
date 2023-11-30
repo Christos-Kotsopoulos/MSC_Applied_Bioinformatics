3deab42ede71d7e0cdc9eac882a3d1c8f4eb8332
Christos-Kotsopoulos <cristos.p.kotsopoulos@gmail.com>
2023-11-30 19:11:11 +0200

    4th Class Directory

---
+++ 4th_Class_Glykos/example4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+
+int length;
+int i=0;
+char seq [ 1000 ];
+
+
+
+	while (scanf("%s",seq) == 1 )
+		{i = i + 1;}	
+
+printf("The total n.o of the input seqs is %d",i);
+
+}
