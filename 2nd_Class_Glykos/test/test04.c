e4c5dbc592860158ed9ca1f3e2278eaad84f57bd
Christos-Kotsopoulos <cristos.p.kotsopoulos@gmail.com>
2023-11-30 19:01:51 +0200

    2nd Class Directory

---
+++ 2nd_Class_Glykos/test/test04.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+
+int length, i;
+
+	char seq [ 1000 ];
+for ( i=0; i <=10 ; i++)
+    {
+	scanf("%s", seq );
+	length = strlen( seq );
+	printf("The lenght of your string is %d\n", length );
+
+      }
+
+}
+
---
+++ 2nd_Class_Glykos/test/test05.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main()
+{
+
+int length, i;
+
+	char seq [ 1000 ];
+while ( scanf("%s", seq ) == 1)
+{
+	length = strlen( seq );
+	printf("The lenght of your string is %d\n", length );
+
+      }
+
+}
+
